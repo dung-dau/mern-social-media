acbf1576424476958ffd31f2878d70332c6bf07a
Dung Dau <dudau0924222@gmail.com>
2022-05-05 14:37:09 -0400

    implement posting functionality

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -6,7 +6,6 @@ export const getPosts = async (req, res) => {
         res.status(200).json(postMessages);
     } catch(error) {
         res.status(404).json({ message: error.message });
-
     }
 }
 
